95c4207d64ef58094ba8570bc47a84f2b9e33ef8
Nate St. George <natestgeorge@gmail.com>
2019-10-24 11:29:18 -0600

    Remove unnecessary check, add comments

--- rotatewm.pl
+++ rotatewm.pl
@@ -7,14 +7,12 @@ $maxDesktop = `wmctrl -d | wc -l`;
 
 $newDesktop = $currentDesktop+$ARGV[0];
 
+# Check for incorrect number of arguments
 if($#ARGV > 1 || $#ARGV < 0) {
 	print($usage);
 }
 
-if ($ARGV[0] == 0) {
-	exit;
-}
-
+# Checks for invalid desktop changes, then switches desktop
 if ($newDesktop < $maxDesktop && $newDesktop >= 0) {
 	system("wmctrl -s $newDesktop");
 }
